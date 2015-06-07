cf40124c254afb9bd80845e07098694c1241ab06
Gyuwon Yi <gyuwon@tidcorp.com>
2015-06-07 16:16:04 +0900

    HelloWorld 솔루션을 추가합니다.

---
+++ HelloWorld/Api/HelloWorldApi/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+
+        private static void Main(string[] args)
+        {
+            var address = "http://localhost:9000";
+            using (WebApp.Start<Startup>(url: address))
+            {
+                Console.WriteLine("Listening on '{0}'...", address);
+                Console.WriteLine("Press Enter key to quit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
