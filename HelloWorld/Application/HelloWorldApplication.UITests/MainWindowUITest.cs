f15f05972538fc280c89ca40d081bf02300f48d6
Gyuwon Yi <gyuwon@tidcorp.com>
2015-06-07 22:53:13 +0900

    UI 테스트 WhenClickGetMessageButtonThenSetsMessageText를 추가합니다.

---
+++ HelloWorld/Application/HelloWorldApplication.UITests/MainWindowUITest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld.Tests
+{
+    [CodedUITest]
+    public class MainWindowUITest
+    {
+        private ApplicationUnderTest _aut;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _aut = ApplicationUnderTest.Launch(
+                typeof(MainWindow).Assembly.ManifestModule.Name);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _aut.Close();
+        }
+
+        [TestMethod]
+        public void WhenClickGetMessageButtonThenSetsMessageText()
+        {
+            // Arrange
+            this.UIMap.ClearMessageText();
+
+            // Act
+            this.UIMap.ClickGetMessageButton();
+
+            // Assert
+            Assert.AreEqual("Hello World",
+                this.UIMap.UIHelloWorldWindow.UIMessageTextBoxEdit.Text);
+        }
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
